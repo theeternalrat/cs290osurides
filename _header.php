643672eebe5850759f8a18eb57e2a39b46e3c799
atkinson137 <atkinson21@gmail.com>
2016-01-26 16:45:09 -0800

    I did more things

--- _header.php
+++ _header.php
@@ -2,11 +2,13 @@
 echo "<html><head>"; ?>
 
 	<title>OSU Rides</title>
+		<script src="https://code.jquery.com/jquery-1.9.1.min.js"></script>
 		<link rel="stylesheet" type="text/css" href="mainstyle.css">
+		
 	</head><body>
 	  
 <ul id="navbar" class="center">
-	<li><a href="index.php" >Home</a></li>
-	<li><a href="settings.php">Settings</a></li>
-	<li><a href="browse.php">Browse</a></li>
+	<li id="mainpage"><a href="index.php" >Home</a></li>
+	<li id="settingspage"><a href="settings.php">Settings</a></li>
+	<li id="browsepage"><a href="browse.php">Browse</a></li>
 </ul>
--- browse.php
+++ browse.php
@@ -1,5 +1,12 @@
 <?php
 include("_header.php");
 echo "<h1>This is the browse page!</h1>";
+?>
+<script type="text/javascript">
+	$(document).ready( function() {
+		document.getElementById("browsepage").className += "active";
+	});
+</script>
+<?php
 include("_footer.php");
 ?>
