a005f47da16ce1d0f4f2eec1b2976f9c3ac937a3
atkinson137 <atkinson21@gmail.com>
2016-01-28 17:31:55 -0800

    Did login things

--- _header.php
+++ _header.php
@@ -1,12 +1,49 @@
 <?php
-echo "<html><head>"; ?>
+session_start();
 
+function checkAuth($doRedirect) {
+	if (isset($_SESSION["onidid"]) && $_SESSION["onidid"] != "") return $_SESSION["onidid"];
+
+	 $pageURL = 'http';
+	 if (isset($_SERVER["HTTPS"]) && $_SERVER["HTTPS"] == "on") {$pageURL .= "s";}
+	 $pageURL .= "://";
+	 if ($_SERVER["SERVER_PORT"] != "80") {
+	  $pageURL .= $_SERVER["SERVER_NAME"].":".$_SERVER["SERVER_PORT"].$_SERVER["SCRIPT_NAME"];
+	 } else {
+	  $pageURL .= $_SERVER["SERVER_NAME"].$_SERVER["SCRIPT_NAME"];
+	 }
+
+	$ticket = isset($_REQUEST["ticket"]) ? $_REQUEST["ticket"] : "";
+
+	if ($ticket != "") {
+		$url = "https://login.oregonstate.edu/cas/serviceValidate?ticket=".$ticket."&service=".$pageURL;
+		$html = file_get_contents($url);
+		$pattern = '/\\<cas\\:user\\>([a-zA-Z0-9]+)\\<\\/cas\\:user\\>/';
+		preg_match($pattern, $html, $matches);
+		if ($matches && count($matches) > 1) {
+			$onidid = $matches[1];
+			$_SESSION["onidid"] = $onidid;
+			return $onidid;
+		} 
+	} else if ($doRedirect) {
+		$url = "https://login.oregonstate.edu/cas/login?service=".$pageURL;
+		echo "<script>location.replace('" . $url . "');</script>";
+	} 
+	return "";
+}
+
+echo "<html><head>"; ?>
 	<title>OSU Rides</title>
+		<script src="https://code.jquery.com/jquery-1.9.1.min.js"></script>
 		<link rel="stylesheet" type="text/css" href="mainstyle.css">
+		
 	</head><body>
-	  
-<ul id="navbar" class="center">
-	<li><a href="index.php" >Home</a></li>
-	<li><a href="settings.php">Settings</a></li>
-	<li><a href="browse.php">Browse</a></li>
-</ul>
+	<div id="container">
+		<div class="header">
+			<ul id="navbar" class="center">
+				<li id="mainpage"><a href="index.php" >Home</a></li>
+				<li id="settingspage"><a href="settings.php">Settings</a></li>
+				<li id="browsepage"><a href="browse.php">Browse</a></li>
+			</ul>
+		</div>
+		<div class="content">
--- settings.php
+++ settings.php
@@ -1,5 +1,16 @@
 <?php
 include("_header.php");
-echo "<h1>This is the settings page!</h1>";
+
+if(checkAuth(true) != ""){
+
+echo "<div class=\"main\"><h1>This is the settings page! You've been authenticated.</h1></div>";
+?>
+<script type="text/javascript">
+	$(document).ready( function() {
+		document.getElementById("settingspage").className += "active";
+	});
+</script>
+<?php
+}
 include("_footer.php");
 ?>
