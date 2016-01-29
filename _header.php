0f0db298be0879727cf242bbe2c7649d32b069b7
atkinson137 <atkinson21@gmail.com>
2016-01-28 17:09:05 -0800

    Added log in

--- _header.php
+++ _header.php
@@ -1,4 +1,38 @@
 <?php
+session_start();
+
+function auth($redir){
+	if(isset($_SESSION["onidid"]) && $_SESSION["onidid"] != "" return $_SESSION["onidid"];
+	
+	$pageURL = 'http';
+	if(isset($_SERVER["HTTPS"]) && $_SERVER["HTTPS"] == "on"){
+		$pageURL .= "s";
+	}
+	
+	if($_SERVER["SERVER_PORT"] != "80"){
+		$pageURL .= $_SERVER["SERVER_NAME"].":".$_SERVER["SERVER_PORT"].$_SERVER["SCRIPT_NAME"];
+	} else {
+		$pageURL .= $_SERVER["SERVER_NAME"].$_SERVER["SCRIPT_NAME"];
+	}
+	
+	$ticket = isset($_REQUEST["ticket"]) ? $_REQUEST["ticket"] : "";
+	
+	if($ticket != ""){
+		$url = "https://login.oregonstate.edu/cas/serviceValidate?ticket=".$ticket."&service=".$pageURL;
+		$html = file_get_contents($url);
+		$pattern = '/\\<cas\\:user\\>([a-zA-Z0-9]+)\\<\\/cas\\:user\\>/';
+		preg_match($pattern, $html, $matches);
+		if($matches && count($matches) > 1){
+			$onidid = $matches[1];
+			$_SESSION["onidid"] = $onidid;
+			return $onidid;
+		}
+	} else if($redir){
+		$url = "https://login.oregonstate.edu/cas/login?service=".$pageURL;
+		echo "<script>location.replace('" . $url . "');</script>";
+	}
+	return "";
+}
 echo "<html><head>"; ?>
 
 	<title>OSU Rides</title>
