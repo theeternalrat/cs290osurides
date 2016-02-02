7ad5bd0d9e5fb6a71700204e20f394e83d255afe
Charles Koll <charleskoll@7cd1c3916424-185.wireless.oregonstate.edu>
2016-02-02 15:22:09 -0800

    Fix syntax and database errors

--- add_review_recieve.php
+++ add_review_recieve.php
@@ -1,12 +1,15 @@
 <?php
 include("_header.php");
 include("db_init.php");
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 ?>
 
 <h1>Saving your submission...</h1>
 
 <?php
-if (isset($_POST["recommend"]){
+if (isset($_POST["recommend"])){
   $recommend         = $_POST["recommend"];
 
 	$errorMsg = "";
@@ -17,7 +20,7 @@ if (isset($_POST["recommend"]){
 	if ($errorMsg == "") {
 		// ok, we can just insert the record
     //TODO bind id
-		if ($stmt = $mysqli->prepare("insert into users (pk_id, recommend) values(1,?)")) {
+		if ($stmt = $mysqli->prepare("insert into reviews (pk_id, recommend) values(1,?)")) {
 			$stmt->bind_param("i", $recommend);
 	    $stmt->execute();
 			$stmt->close();
