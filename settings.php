5b15d51a26084e8c4c1535f5b436b57dcacef8c9
Charles Koll <kollch@oregonstate.edu>
2016-02-04 15:29:18 -0800

    Clean up alignment

--- settings.php
+++ settings.php
@@ -1,17 +1,16 @@
 <?php
 include("_header.php");
 
-if(checkAuth(true) != ""){
-
-echo "<div class=\"main\"><h1>This is the settings page! You've been authenticated.</h1></div>";
-echo "<h1>This is the settings page!</h1>";
-?>
-<script type="text/javascript">
-	$(document).ready( function() {
-		document.getElementById("settingspage").className += "active";
-	});
-</script>
-<?php
+if (checkAuth(true) != "") {
+	echo "<div class=\"main\"><h1>This is the settings page! You've been authenticated.</h1></div>";
+	echo "<h1>This is the settings page!</h1>";
+	?>
+	<script type="text/javascript">
+		$(document).ready( function() {
+			document.getElementById("settingspage").className += "active";
+		});
+	</script>
+	<?php
 }
 include("_footer.php");
 ?>
