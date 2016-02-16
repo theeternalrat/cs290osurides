58560a15c0c7b0c4afe0611858e2c13792470cfd
adpextwindong <adpextwindng@gmail.com>
2016-02-15 18:23:04 -0800

    Add 'required' attribute to add_review input forms
    Add 'required' attribute to add_review input forms

--- add_review.php
+++ add_review.php
@@ -4,11 +4,11 @@
 
 <form method="post" action='add_review_recieve.php' class="inform">
 <ul>
-<li><label>ID:</label> <input type="number" name="id">
-<li><label>Driver or Passenger:</label> <input type="text" name="driverorpassenger">
-<li><label>Score:</label> <input type="number" name="user_score">
-<li><label>Recommend?:</label> <input type="checkbox" name="recommend">
-<li><label>Description:</label> <input type="text" name="description">
+<li><label>ID:</label> <input type="number" name="id" required>
+<li><label>Driver or Passenger:</label>
+<li><label>Score:</label> <input type="number" name="user_score" required>
+<li><label>Recommend?:</label> <input type="checkbox" name="recommend" required>
+<li><label>Description:</label> <input type="text" name="description" required>
 <li><input type=submit>
 </ul>
 </form>
