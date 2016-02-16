d8d9d2bab21eb5422386762ed5c0c37dac9eeac5
adpextwindong <adpextwindng@gmail.com>
2016-02-15 18:26:25 -0800

    Change driver enum input field to select form
    Change driver enum input field to select form

--- add_review.php
+++ add_review.php
@@ -6,6 +6,10 @@
 <ul>
 <li><label>ID:</label> <input type="number" name="id" required>
 <li><label>Driver or Passenger:</label>
+<select name="Spot">
+ <option value="DRIVER">Driver</option>
+ <option value="PASSENGER">Passenger</option>
+</select>
 <li><label>Score:</label> <input type="number" name="user_score" required>
 <li><label>Recommend?:</label> <input type="checkbox" name="recommend" required>
 <li><label>Description:</label> <input type="text" name="description" required>
