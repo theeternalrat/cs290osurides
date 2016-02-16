8e1bf6d4d042a840d3f47e8b48777f08b8280328
adpextwindong <adpextwindng@gmail.com>
2016-02-16 14:24:05 -0800

    Hotfix _header.php incomplete merge bugs
    Hotfix _header.php incomplete merge bugs

--- _header.php
+++ _header.php
@@ -1,87 +1,11 @@
 <?php include("auth.php"); ?>
 
-<<<<<<< HEAD
-
-function checkAuth($doRedirect) {
-	if (isset($_SESSION["onidid"]) && $_SESSION["onidid"] != "") return $_SESSION["onidid"];
-
-	 $pageURL = 'http';
-	 if (isset($_SERVER["HTTPS"]) && $_SERVER["HTTPS"] == "on") {$pageURL .= "s";}
-	 $pageURL .= "://";
-	 if ($_SERVER["SERVER_PORT"] != "80") {
-	  $pageURL .= $_SERVER["SERVER_NAME"].":".$_SERVER["SERVER_PORT"].$_SERVER["SCRIPT_NAME"];
-	 } else {
-	  $pageURL .= $_SERVER["SERVER_NAME"].$_SERVER["SCRIPT_NAME"];
-	 }
-	 
-function auth($redir){
-	if(isset($_SESSION["onidid"]) && $_SESSION["onidid"] != ""){
-		 return $_SESSION["onidid"];
-	}
-	
-	$pageURL = 'http';
-	if(isset($_SERVER["HTTPS"]) && $_SERVER["HTTPS"] == "on"){
-		$pageURL .= "s";
-	}
-	
-	if($_SERVER["SERVER_PORT"] != "80"){
-		$pageURL .= $_SERVER["SERVER_NAME"].":".$_SERVER["SERVER_PORT"].$_SERVER["SCRIPT_NAME"];
-	} else {
-		$pageURL .= $_SERVER["SERVER_NAME"].$_SERVER["SCRIPT_NAME"];
-	}
-	
-	$ticket = isset($_REQUEST["ticket"]) ? $_REQUEST["ticket"] : "";
-	
-	if($ticket != ""){
-		$url = "https://login.oregonstate.edu/cas/serviceValidate?ticket=".$ticket."&service=".$pageURL;
-		$html = file_get_contents($url);
-		$pattern = '/\\<cas\\:user\\>([a-zA-Z0-9]+)\\<\\/cas\\:user\\>/';
-		preg_match($pattern, $html, $matches);
-		if($matches && count($matches) > 1){
-			$onidid = $matches[1];
-			$_SESSION["onidid"] = $onidid;
-			return $onidid;
-		}
-	} else if($redir){
-		$url = "https://login.oregonstate.edu/cas/login?service=".$pageURL;
-		echo "<script>location.replace('" . $url . "');</script>";
-	}
-	return "";
-}
- 
-
-	$ticket = isset($_REQUEST["ticket"]) ? $_REQUEST["ticket"] : "";
-
-	if ($ticket != "") {
-		$url = "https://login.oregonstate.edu/cas/serviceValidate?ticket=".$ticket."&service=".$pageURL;
-		$html = file_get_contents($url);
-		$pattern = '/\\<cas\\:user\\>([a-zA-Z0-9]+)\\<\\/cas\\:user\\>/';
-		preg_match($pattern, $html, $matches);
-		if ($matches && count($matches) > 1) {
-			$onidid = $matches[1];
-			$_SESSION["onidid"] = $onidid;
-			return $onidid;
-		} 
-	} else if ($doRedirect) {
-		$url = "https://login.oregonstate.edu/cas/login?service=".$pageURL;
-		echo "<script>location.replace('" . $url . "');</script>";
-	} 
-	return "";
-}
-
-echo "<html><head>"; ?>
-=======
 <html><head>
->>>>>>> develop
 	<title>OSU Rides</title>
 		<script src="https://code.jquery.com/jquery-1.9.1.min.js"></script>
 		<link rel="stylesheet" type="text/css" href="mainstyle.css">
-
+		
 	</head><body>
-<<<<<<< HEAD
-
-=======
->>>>>>> develop
 	<div id="container">
 		<div class="header">
 			<ul id="navbar" class="center">
@@ -97,13 +21,3 @@ echo "<html><head>"; ?>
 			</ul>
 		</div>
 		<div class="content">
-<<<<<<< HEAD
-	  
-<ul id="navbar" class="center">
-	<li id="Name" class="name"> OSU Rides</li> 
-	<li id="mainpage"><a href="index.php" >Home</a></li>
-	<li id="settingspage"><a href="settings.php">Settings</a></li>
-	<li id="browsepage"><a href="browse.php">Browse</a></li>
-</ul>
-=======
->>>>>>> develop
