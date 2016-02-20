97a0ec6a8382e185393c4795e093bd5aa52549c2
leian7 <leian@oregonstate.edu>
2016-02-20 14:29:16 -0800

    bio character limit and nickname character validation

--- signup.php
+++ signup.php
@@ -1,8 +1,47 @@
+<html>
+<head>
+<style>
+.error {color:#FF0000;}
+</style>
+</head>
+<body>
+
 <?php
 include("_header.php");
+
+//error flags:
+$nickErr = $bioErr = "";
+//vars
+$nickname = $bio = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+	$nickname = test_input($_POST["nickname"]);
+	if (!preg_match("/^[A-Za-z0-9_-]+$/", $nickname)){
+		$nickErr = "Only letters, numbers, underscores/dashes allowed";
+		echo "nay";
+	}
+	else {
+		echo "good nick";
+	} 
+	if (empty($_POST["bio"])) {
+		$bio = "";
+	}
+	else {
+		$bio = test_input($_POST["bio"]);
+	}
+}
+
+//strips preceding/trailing whitespace/slashes + does htmlspecialchars
+function test_input($data) {
+	$data = trim($data);
+	$data = stripslashes($data);
+	$data = htmlspecialchars($data);
+	echo "test: i'm in test_input";
+	return $data;
+}
 ?>
 
-
+<!-- Hides seats dropdown if passenger -->
 <script type="text/javascript">
 function statusCheck() {
 	if(document.getElementById('driverCheck').checked || document.getElementById('eitherCheck').checked) {
@@ -17,9 +56,8 @@ function statusCheck() {
 <h3>You do not yet have an account. Please fill in the information below and click 'Continue'</h3>
 <p>User Information:</p>
 <form action="signup.php" method="POST">
-	Nickname: <input autofocus required type="text" name="nickname">
+	Nickname: <input autofocus required type="text" name="nickname" value="<?php echo $nick;?>"><span class="error"><?php echo $nickErr;?></span>
 	<br>
-	<!-- Note: maybe change status values to easy ints? -->
 	I would like to be a: <br>
 	<input type="radio" name="status" value="Driver" onclick="javascript:statusCheck();" id="driverCheck" required>Driver<br>
 	<input type="radio" name="status" value="Passenger" onclick="javascript:statusCheck();" id="passCheck">Passenger<br>
@@ -39,23 +77,20 @@ function statusCheck() {
 	</select>
 	</div>
 	Bio:<br>
-	<textarea rows="4" cols="50" placeholder="Enter a bit about yourself." name="bio"></textarea>
+	<textarea rows="4" cols="50" placeholder="Enter a bit about yourself." name="bio" maxlength="1000"><?php echo $bio;?></textarea>
 	<br>
 	<input type="checkbox" name="emailterms" value="eterms" required> I agree to let OSURides access my OSU provided information and use my OSU email for alerts.
 	<br>
-	<input type="submit" name="continue" value="Continue">
-	
+	<input type="submit" name="continue" value="Continue">	
 </form>
-<?php
 
-	$nickname =  htmlspecialchars($_POST["nickname"]);
+<?php
 	$status = 0;
 	$seats = 0;
 	if($_POST["status"] === "Driver" || $_POST["status"] === "Either"){
 		$status = 1;
 		$seats = $_POST["seats"];
 	}
-	$bio = htmlspecialchars($_POST["bio"]);
 	
 	getAdvanced();
 	
@@ -65,9 +100,9 @@ function statusCheck() {
 		$results->bind_param("issssii", $onid, $avatar, $name, $nickname, $bio, $email, $status, $seats);
 		$results->execute();
 	}
-	
-	
-
 
 include("_footer.php");
 ?>
+
+</body>
+</html>
