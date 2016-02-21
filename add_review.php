27a3502531180fdfa72422223690c840db61f91a
leian7 <leian@oregonstate.edu>
2016-02-21 14:20:31 -0800

    ID can't be negative and recommend box not required

--- add_review.php
+++ add_review.php
@@ -4,14 +4,14 @@
 
 <form method="post" action='add_review_recieve.php' class="inform">
 <ul>
-<li><label>ID:</label> <input type="number" name="id" required>
+<li><label>ID:</label> <input type="number" name="id" oninput="validity.valid||(value='');" min=0 required>
 <li><label>Driver or Passenger:</label>
 <select name="Spot">
  <option value="DRIVER">Driver</option>
  <option value="PASSENGER">Passenger</option>
 </select>
 <li><label>Score:</label> <input type="number" name="user_score" required>
-<li><label>Recommend?:</label> <input type="checkbox" name="recommend" required>
+<li><label>Recommend?:</label> <input type="checkbox" name="recommend">
 <li><label>Description:</label> <input type="text" name="description" required>
 <li><input type=submit>
 </ul>
