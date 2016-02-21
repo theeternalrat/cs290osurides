c48efa7be6a2f8e5cfb4aba167844cb6ebcd3ac8
kollch <charles@kollonline.com>
2016-02-20 16:40:56 -0800

    Change class of filler image

--- index.php
+++ index.php
@@ -1,11 +1,10 @@
 <?php include("_header.php"); ?>
-
-	<div class= "info">
-		<img src="images/filler.png" alt="filler for future map" class ="filler-image">
+	<div class="info">
 		<h1>OSU Rides</h1>
 		<p>We are a team of student programmers currently working on completing a website for our CS 290 course. Our group consists of: Ryan Atkinson, Samuel Morey, Takumi Crary, Rebecca Farnham, Charles Koll, and Annie Lei. We hope you enjoy our site!</p>
-	</div>
-	<div class= "login-filler">
+	</div> 
+	<img src="images/filler.png" alt="filler for future map" class="filler-image">
+	<div class="login-filler">
 		<a href="settings.php">Log In</a>
 	</div>
 <script type="text/javascript">
