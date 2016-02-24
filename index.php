81aa60a568768327637dc43adc130ea95230a442
atkinson137 <atkinson21@gmail.com>
2016-02-23 16:33:38 -0800

    ADDED A PICTURE SAM

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 		<h1>OSU Rides</h1>
 		<p>We are a team of student programmers currently working on completing a website for our CS 290 course. Our group consists of: Ryan Atkinson, Samuel Morey, Takumi Crary, Rebecca Farnham, Charles Koll, and Annie Lei. We hope you enjoy our site!</p>
 	</div> 
-	<img src="images/filler.png" alt="filler for future map" class="filler-image">
+	<img src="imgs/filler.jpg" alt="filler for future map" class="filler-image">
 	<?php  if(checkAuth(false) != "") {
 		echo "<input type=\"button\" class=\"login-filler\" onclick=\"location.href='logout.php';\" value=\"Log Out\" />";
 	} else {
