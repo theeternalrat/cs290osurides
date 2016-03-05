45f68d0ef4932a4432b72363bfaba1ef0822541e
leian7 <leian@oregonstate.edu>
2016-03-05 14:45:11 -0800

    added 'details' section

--- listings.php
+++ listings.php
@@ -6,7 +6,7 @@ include("db_init.php");
 <div id=rides>
   <?php
   echo "<table class='rides'><tr><th>Carpool Id<th>Leave Date<th>From Latitude<th>From Longitude<th>Destination Latitude<th>Destination Longitude<th></tr>";
-  if ($result_rides_stmt = $mysqli->prepare("SELECT `carpool_id`, `leave_date`, `from_lat`, `from_long`, `destination_lat`, `destination_long` FROM `rides` WHERE open_to_passengers = TRUE")) {
+  if ($result_rides_stmt = $mysqli->prepare("SELECT `carpool_id`, `leave_date`, `from_lat`, `from_long`, `destination_lat`, `destination_long`, `details`, FROM `rides` WHERE open_to_passengers = TRUE")) {
       $result_rides_stmt->execute();
       $result_rides = $result_rides_stmt->get_result();
       while($obj = $result_rides->fetch_object()){
@@ -17,6 +17,7 @@ include("db_init.php");
               echo "<td>".htmlspecialchars($obj->from_long)."</td>";
               echo "<td>".htmlspecialchars($obj->destination_lat)."</td>";
               echo "<td>".htmlspecialchars($obj->destination_long)."</td>";
+			  echo "<td>".htmlspecialchars($obj->details)."</td>";
               echo "</tr>";
       }
       $result_rides_stmt->close();
