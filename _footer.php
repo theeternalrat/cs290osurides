3b37c0ba45f3cc5100c289849e1abd491cac60c0
kollch <charles@kollonline.com>
2016-03-09 23:26:33 -0800

    Use renamed image

--- _footer.php
+++ _footer.php
@@ -1,7 +1,7 @@
 <footer>	
 	<ul>
 		<li>
-			<a href = "https://github.com/SamMorey"><img src="imgs/test.jpg" alt="Sam's GitHub" class= "team"></a>
+			<a href = "https://github.com/SamMorey"><img src="imgs/fly_sam.jpg" alt="Sam's GitHub" class= "team"></a>
 		</li>
 		<li>
 			<a href = "https://github.com/kollch"><img src="imgs/github.svg" alt="Charles's GitHub" class= "team"></a>
