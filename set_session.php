fe04bd6d0d9f8b4378ef80977c96ec2c1c6f74fd
atkinson137 <atkinson21@gmail.com>
2016-03-10 04:24:35 -0800

    changed user raiting

---
+++ set_session.php
@@ -0,0 +1,15 @@
+<?php
+require_once("db_init.php");
+
+if(!isset($_SESSION["uid"])){
+			$sql = "SELECT pk_id FROM `users` WHERE onid_id=?";
+			if($stmt = $mysqli->prepare($sql)){
+				$stmt->bind_param("s", $_SESSION["onidid"]);
+				$stmt->execute();
+				$stmt->bind_result($uid);
+				$stmt->fetch();
+				$stmt->close();
+				
+				$_SESSION["uid"] = $uid;
+			}
+}
